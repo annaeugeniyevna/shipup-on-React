04651f4ebac1420aa5f70ef829421f147bbbbffd
Anna <annapaschenko828@gmail.com>
2026-04-30 17:33:54 +0300

    feat: add responsive styles for operation

--- src/components/Operation.jsx
+++ src/components/Operation.jsx
@@ -1,19 +1,26 @@
 import truckIcon from '../assets/images/operation-car.svg';
+import decorOperation from '../assets/images/operation-decor.svg';
 import courierPhoto from '../assets/images/operation-courier.png';
+import courierBigger from '../assets/images/operation-courier-bigger.png';
 import truckDriver from '../assets/images/operation-truck-driver.png';
 import fastShipping from '../assets/images/operation-fast-shipping.png';
 
 const Operation = () => {
     return (
-        <section className="mt-20 mb-20">
-            <div className="max-w-[1440px] mx-auto px-[5%]">
+        <section className="mt-20 mb-20 lg:relative lg:pt-62 lg:mt-0 lg:mb-25">
+            <div className="max-w-[1440px] mx-auto px-[5%] lg:px-[6%]">
                 <img 
                 src={truckIcon} 
                 alt="Delivery truck"
-                className="w-28 md:w-45"/>
+                className="w-28 md:w-45 lg:w-50 lg:absolute lg:top-29"/>
                 <h2 className="font-sans font-regular text-2xl text-center text-black mb-20 md:text-4xl"><span className="font-sans font-bold text-2xl text-black mr-2 border-b-4 border-secondary pb-2 md:text-4xl">Operation</span> Mode</h2>
-                <div className="flex flex-col gap-8">
-                    <div className="md:flex md:items-center">
+                <img 
+                className="hidden lg:block lg:absolute lg:left-[370px] lg:bottom-[330px]"
+                src={decorOperation} 
+                alt="Decor"/>
+                <div className="flex flex-col gap-8 lg:gap-14">
+                    <div className="md:flex md:items-center 
+                    lg:gap-43 lg:justify-center lg:ml-27">
                         <div>
                             <div className="flex items-center gap-4 mb-7 md:gap-7">
                                 <span className="font-sans font-bold text-2xl text-off-white bg-dark-purple rounded-full py-2 px-4">1</span>
@@ -25,11 +32,17 @@ const Operation = () => {
                         </div>
                         <div>
                             <img 
+                            className="lg:w-[600px] lg:hidden"
                             src={courierPhoto}
+                            alt="Courier"/>
+                            <img 
+                            className="hidden lg:block"
+                            src={courierBigger} 
                             alt="Courier"/>
                         </div>
                     </div>
-                    <div className="md:flex md:items-center">
+
+                    <div className="md:flex md:items-center lg:gap-65 lg:justify-center">
                         <div className="md:order-2">
                             <div className="flex items-center gap-4 mb-7 md:gap-7">
                                 <span className="font-sans font-bold text-2xl text-off-white bg-dark-purple rounded-full py-2 px-4">2</span>
@@ -41,11 +54,12 @@ const Operation = () => {
                         </div>
                         <div className="md:order-1">
                             <img
+                            className="lg:w-[400px]"
                             src={truckDriver} 
                             alt="Truck Driver"/>
                         </div>
                     </div>
-                    <div className="md:flex md:items-center">
+                    <div className="md:flex md:items-center lg:gap-65 lg:justify-center lg:ml-10">
                         <div>
                             <div className="flex items-center gap-4 mb-7 md:gap-7">
                                 <span className="font-sans font-bold text-2xl text-off-white bg-dark-purple rounded-full py-2 px-4">3</span>
@@ -57,6 +71,7 @@ const Operation = () => {
                         </div>
                         <div>
                             <img
+                            className="lg:w-[400px]"
                             src={fastShipping} 
                             alt="Woman on a moped"/>
                         </div>
